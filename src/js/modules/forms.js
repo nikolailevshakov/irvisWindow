72e291fa6cf54712beb16de888d9da25462e09f8
Levshakov Nikolai <levshakov_nikk@bk.ru>
2020-04-13 22:19:39 +0300

    add forms and connection with server, number validation

---
+++ src/js/modules/forms.js
@@ -0,0 +1,63 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input'),
+          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+
+        phoneInputs.forEach(item => {
+            item.addEventListener('input', ()=> {
+                item.value = item.value.replace(/\D/, '');
+            });
+        });
+    
+    const message = {
+        lading: 'Загрузка...',
+        success: 'Спасибо! Скоро с вами свяжутся',
+        failure: 'Что-то пошло не так...'
+    };
+// добавили async/await, поэтому скрипт ждет ответ от сервера и let Не будет Undefined
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        // fetch api
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+// Тоже подождет получение ответа от сервера
+        return await res.text();
+    };
+// очистка инпутов после использования
+    const clearInputs = () => {
+        inputs.forEach(item => {
+            item.value = '';
+        });
+    }
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+// Для запроса
+            const formData = new FormData(item);
+// вернется промис с сервера
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.success;
+                })
+                .catch(()=> {
+                    statusMessage.textContent = message.failure;
+                })
+                .finally(()=>{
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+        });
+    });
+};
+
+export default forms;
